f21239bd9ab9f594e5a64cabbf0f13a76487fca0
Grieco <grieco@intre.it>
2020-07-31 16:04:44 +0200

    Updated header propagator

---
+++ CustomPlugin/MiaHeadersPropagator.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Service.Environment;
+
+namespace CustomPlugin
+{
+    public class MiaHeadersPropagator
+    {
+        private readonly Dictionary<string, string> _headers = new Dictionary<string, string>();
+        public Dictionary<string, string> Headers => _headers;
+        private MiaEnvConfiguration _envConfig;
+
+        public MiaHeadersPropagator(IHeaderDictionary headers, MiaEnvConfiguration envConfig)
+        {
+            _envConfig = envConfig;
+            Headers[_envConfig.USERID_HEADER_KEY] = headers[_envConfig.USERID_HEADER_KEY];
+            Headers[_envConfig.GROUPS_HEADER_KEY] = headers[_envConfig.GROUPS_HEADER_KEY];
+            Headers[_envConfig.CLIENTTYPE_HEADER_KEY] = headers[_envConfig.CLIENTTYPE_HEADER_KEY];
+            Headers[_envConfig.BACKOFFICE_HEADER_KEY] = headers[_envConfig.BACKOFFICE_HEADER_KEY];
+        }
+
+        public string GetUserId()
+        {
+            return Headers[_envConfig.BACKOFFICE_HEADER_KEY];
+        }
+
+        public string GetGroups()
+        {
+            return Headers[_envConfig.GROUPS_HEADER_KEY];
+        }
+
+        public string GetClientType()
+        {
+            return Headers[_envConfig.CLIENTTYPE_HEADER_KEY];
+        }
+
+        public bool IsFromBackOffice()
+        {
+            bool result;
+            try
+            {
+                result = bool.Parse(Headers[_envConfig.BACKOFFICE_HEADER_KEY]);
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
