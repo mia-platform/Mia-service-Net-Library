0d5fad45198417e3962f3d43348daaeaf51beebe
Grieco <grieco@intre.it>
2020-09-28 11:08:41 +0200

    Return stringified package version

--- CustomPlugin/CpStatusBody.cs
+++ CustomPlugin/CpStatusBody.cs
@@ -6,7 +6,7 @@ namespace CustomPlugin
     {
         public string Name => System.Reflection.Assembly.GetEntryAssembly()?.GetName().Name;
 
-        public Version Version => System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version;
+        public string Version => System.Reflection.Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
 
         public string Status { get; set; }
     }
